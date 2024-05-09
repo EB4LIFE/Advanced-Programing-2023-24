2c1b4ef7138a56f6b035baaa014ae8b8f5d8bfee
EB4LIFE <105313463+EB4LIFE@users.noreply.github.com>
2024-05-09 15:09:57 +0300

    Update EBAPHW1Rational.h

--- EBAPHW1Rational.h
+++ EBAPHW1Rational.h
@@ -4,10 +4,9 @@
 #include <iostream>
 using namespace std;
 
-
 class Rational {
-
-private: //by default
+private:
+    //by default
     int numerator;
     int denominator;
 
@@ -16,83 +15,85 @@ public:
     //An assignment constructor (ctor) with parameters that default to 0 for
     // the numerator and 1 for the denominator.The implementation must 
     // contain the following output : print: in constructor
-    Rational(int num = 0, int denom = 1) : numerator(num), denominator(denom) {
+    Rational(int num = 0, int den = 1) : numerator(num), denominator(den) {
         cout << "print: in constructor" << endl;
     }
     //copy constructor
-    Rational(const Rational& other): numerator(other.numerator), denominator(other.denominator)  {
+    Rational(const Rational& other) : numerator(other.numerator), denominator(other.denominator) {
         cout << "print: in copy-constructor" << endl;
     }
     //set numerator
     void setNumerator(int num) {
         numerator = num;
     }
-    //get numerator
-    int getNumerator() const {
-        return numerator;
-    }
-     //set denominator
-    void setDenominator(int denom) {
-        if (denom == 0) {
+    //set numerator
+    void setDenominator(int den) {
+        if (den == 0) {
             denominator = 1;
         }
         else {
-            denominator = denom;
-            if (denominator < 0) {
-                numerator *= -1;
-                denominator *= -1;
-            }
+            denominator = den;
+        }
+        if (denominator < 0) {
+            numerator = -numerator;
+            denominator = -denominator;
         }
     }
-    //get denominaotr
+    //get num
+    int getNumerator() const {
+        return numerator;
+    }
+    //get denom
     int getDenominator() const {
         return denominator;
     }
     //print rational
-// Method to print the rational number
+    // Method to print the rational number
     void print() const {
-        cout << numerator << "/" << denominator;
+        cout << numerator << "/" << denominator << endl;
     }
     //equal
     // Method to check equality
     bool equal(const Rational& other) const {
-        return (numerator == other.numerator && denominator == other.denominator);
+        return numerator == other.numerator && denominator == other.denominator;
     }
     //make equals
     // Method to make two rationals equal
-    void makeEquals(const Rational& other) {
-        numerator = other.numerator;
-        denominator = other.denominator;
+    void makeEquals(Rational& other) {
+        other.numerator = numerator;
+        other.denominator = denominator;
     }
     //reduction
     // Helper "stepping stone" function method for reducing the fraction
     void reduce() {
-        int gcd = 1;
-        int smaller = (numerator < denominator) ? numerator : denominator;
-        for (int i = 2; i <= smaller; ++i) {
-            if (numerator % i == 0 && denominator % i == 0) {
-                gcd = i;
-            }
-        }
+        int gcd = getGCD(numerator, denominator);
         numerator /= gcd;
         denominator /= gcd;
-        if (numerator == 0)
-        {
+        if (numerator == 0) {
             denominator = 1;
         }
     }
     //add rational
     Rational add(const Rational& other) const {
         int num = numerator * other.denominator + other.numerator * denominator;
-        int denom = denominator * other.denominator;
-        Rational result(num, denom);
-        result.reduce();
-        return result;
+        int den = denominator * other.denominator;
+        Rational sum(num, den);
+        sum.reduce();
+        return sum;
     }
     //addone
     // Method to add one to the rational
     Rational addOne() const {
-        return add(Rational(1, 1));
+        Rational one(1);
+        return add(one);
+    }
+    //personal thing to make it work
+private:
+    int getGCD(int a, int b) const {
+        if (b == 0) {
+            return a;
+        }
+        return getGCD(b, a % b);
     }
 };
 #endif
