e3e275e2ea9aa935a88d9b5c53dac5f0c5e82ee1
EB4LIFE <105313463+EB4LIFE@users.noreply.github.com>
2024-05-19 03:46:10 -0400

    Update EBAPHW2PointPolygonMain.cpp

--- EBAPHW2PointPolygonMain.cpp
+++ EBAPHW2PointPolygonMain.cpp
@@ -10,39 +10,37 @@ using namespace std;
 void fillPoints(Polygon& shape, int size);
 
 int main () { 
-//the two shapes we will be comparing using object from class polygon
-Polygon Poly1, Poly2;
 //sides of said polygon
 int sides;
 
 cout << "enter number of sides:" << endl;
 cin >> sides;
 //putting it in the assignment ctor
-poly1(sides);
+Polygon Poly1(sides);
 //now getting the points for polygon 1
 cout << "enter the point values:" << endl;
-fillPoints(poly1, sides);
+fillPoints(Poly1, sides);
 
 //now for the second shape
 cout << "enter number of sides:" << endl;
 cin >> sides;
 //putting it in the assignment ctor
-poly2(sides);
+Polygon Poly2(sides);
 //get the point for polygon 2
 cout << "enter the point values:" << endl;
-fillPoints(poly2, sides);
+fillPoints(Poly2, sides);
    
 // Compare polygons and print results
-if (poly1.isIdentical(poly2)) 
+if (Poly1.isIdentical(Poly2)) 
 {
 cout << "equal" << endl;
-cout << round(poly1.perimeter()) << endl;
+cout << "perimeter: " << round(Poly1.perimeter()) << endl;
 } 
 else 
 {
 cout << "not equal" << endl;
-cout << round(poly1.perimeter()) << endl;
-cout << round(poly2.perimeter()) << endl;
+cout << "perimeter: "<< round(Poly1.perimeter()) << endl;
+cout << "perimeter: "<< round(Poly2.perimeter()) << endl;
 }
 return 0;
 }
@@ -55,7 +53,7 @@ for(int i = 0; i < size; ++i)
 {
    //put the x and y and coordinate format
    cin >> par >> x >> par >> y >> par;
-   shape.getPoints().setX(x);
-   shape.getPoints().setY(y);
+   shape.getPoints()->setX(x);
+   shape.getPoints()->setY(y);
 }
 }
