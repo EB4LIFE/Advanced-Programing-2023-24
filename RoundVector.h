a87a7c22365378c4c80caab017718acd29764d44
EB4LIFE <105313463+EB4LIFE@users.noreply.github.com>
2024-05-19 11:27:57 -0400

    Update RoundVector.h

--- RoundVector.h
+++ RoundVector.h
@@ -44,29 +44,55 @@ public:
   cout << "in destructor" << endl;
   }
   //methods 
-  addNext 
-  //an insertion method as described above. The method receives a 
+   //an insertion method as described above. The method receives a 
   //number and inserts it at the tail of the array if the array is not full. If the array is
   //full the following output will be printed: “Vector is full”.
-removeFirst 
-  //a removal method that removes the head element as described 
-  //above. The method is to return the removed element. Note: Assume the array is 
-  //not empty. At this stage no test will be done to check if the array is empty –
-  //such a test will be added after we study exceptions.
- firstValue 
-  //returns the head element without removing it. Note: Assume the
-  //array is not empty. At this stage no test will be done to check if the array is
-  //empty – such a test will be added after we study exceptions.
- isEmpty 
-  //a boolean method the returns true if the array contains no elements.
- clear 
+  addNext(int value) {
+    //meaning full
+    if ((tail + 1) % (capacity + 1) == head) {
+    cout << "Vector is full" << endl;
+    return;
+    }
+    //otherwise...
+    vec[tail] = value;
+    tail = (tail + 1) % (capacity + 1);
+  }
+
+//a removal method that removes the head element as described 
+//above. The method is to return the removed element. 
+removeFirst() {
+  int value = vec[head];
+  head = (head + 1) % (capacity + 1);
+  return value;
+}
+
+//returns the head element without removing it
+ firstValue() const {
+  return vec[head];
+ }
+ //a boolean method the returns true if the array contains no elements.
+ bool isEmpty() {
+   //if head == tail it means it is empty so true
+   return head == tail;
+   }
   //a method that empties the array. It is not required to modify in any way
   //the contents of the array – just zero the head and tail.
- print 
-  //a method that prints the elements contained in the array from the head
+ void clear() {
+   head = 0;
+   tail = 0;
+ }
+
+ //a method that prints the elements contained in the array from the head
   //to the tail. Each two elements are to be separated by a space and the printing is
   //to be terminated by an end line.
-
-};
-
+ void print () const {
+   int i = head;
+   //as long as the index is not the tail we continue to add  number into vec array
+   while (i != tail) {
+            cout << vec[i] << "  ";
+            i = (i + 1) % (capacity + 1);
+        }
+        cout << endl;
+    } 
+ };
 #endif
