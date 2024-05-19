952d2a98c023f5bc5be1f2729468ed29ecc21b9c
EB4LIFE <105313463+EB4LIFE@users.noreply.github.com>
2024-05-19 01:47:26 -0400

    Update EBAPHW2PointPolygonMain.cpp

--- EBAPHW2PointPolygonMain.cpp
+++ EBAPHW2PointPolygonMain.cpp
@@ -5,5 +5,21 @@
 using namespace std;
 
 int main () { 
+   Polygon Poly1, Poly2;
 
+
+
+// Compare polygons and print results
+if (poly1.isIdentical(poly2)) 
+{
+cout << "equal" << endl;
+cout << round(poly1.perimeter()) << endl;
+} 
+else 
+{
+cout << "not equal" << endl;
+cout << round(poly1.perimeter()) << endl;
+cout << round(poly2.perimeter()) << endl;
+}
+return 0;
 }
