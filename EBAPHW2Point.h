00b4734a619387dff3824d2ec1b4cd29e074a350
EB4LIFE <105313463+EB4LIFE@users.noreply.github.com>
2024-05-19 00:58:04 -0400

    Update EBAPHW2Point.h

--- EBAPHW2Point.h
+++ EBAPHW2Point.h
@@ -6,43 +6,52 @@
 class Point {
 
 private:
-        int OGx;
-        int OGy;
+        float OGx;
+        float OGy;
 
 public:
   //Constructor
   Point() {
+   OGx = 0; 
+   OGy = 0;
+}
+ //assignment ctor
+ Point(float x, float y ): OGx(x), OGy(y) {
+}
+//copy ctor
+Point (const Point& P) : OGx(P.OGx), OGy(P.OGy) {
 
 }
-
  //setter methods
  void setX(int x)
 {
   OGx = x;
 }
+
 void setY(int y)
 {
  OGy = y;
 }
+
 //getter methods
-int getX()
+float getX() const
 {
 return OGx;
 }
-int getY()
+float getY() const
 {
 return OGy;
 }
 
-float distance(Point point)
+float distance(const Point& P)
 //method to calculate distance between 
 //2 points (1 called and 1 passed)
 {
     float distance;
-    int x1 = OGx;
- .  int y1 = OGy;
-    int x2 = point.OGx;
-    int y2 = point.OGy;
+    float x1 = OGx;
+ .  float y1 = OGy;
+    float x2 = P.OGx;
+    float y2 = P.OGy;
     distance = sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2));
     return distance;
 }
