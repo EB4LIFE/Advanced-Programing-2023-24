3dcb04ad6c4ed4b6e25d9f47c38f36680ba78274
EB4LIFE <105313463+EB4LIFE@users.noreply.github.com>
2024-05-19 00:20:19 -0400

    Update EBAPHW2Point.h

--- EBAPHW2Point.h
+++ EBAPHW2Point.h
@@ -1,1 +1,37 @@
-//Makers of point - simple
+#pragma once
+#ifndef POINT_H
+#define POINT_H
+
+class Point {
+
+private:
+        int OGx;
+        int OGy;
+
+public:
+
+ //setter methods
+ void setX(int x)
+{
+}
+void setY(int y)
+{
+}
+//getter methods
+int getX()
+{
+}
+int getY()
+{
+}
+
+float distance(Point point)
+//method to calculate distance between 
+//2 points (1 called and 1 passed)
+{
+
+}
+
+};
+
+#endif POINT_H
