cd25916004e0d247e3aeb04006b1e9f3b00762de
EB4LIFE <105313463+EB4LIFE@users.noreply.github.com>
2024-05-19 06:04:44 -0400

    Update EBAPHW2PointPolygonMain.cpp

--- EBAPHW2PointPolygonMain.cpp
+++ EBAPHW2PointPolygonMain.cpp
@@ -1,59 +1,50 @@
 //Eitan Brown 
 //346816549
-#include<iostream>
-#include<cmath>
+#include <iostream>
+#include <cmath>
 #include "Point.h"
 #include "Polygon.h"
 using namespace std;
 
-//will be intliazied below but basically will get the point for the shape of said polygon 
 void fillPoints(Polygon& shape, int size);
 
-int main () { 
-//sides of said polygon
-int sides;
+int main() {
+    int sides;
 
-cout << "enter number of sides:" << endl;
-cin >> sides;
-//putting it in the assignment ctor
-Polygon Poly1(sides);
-//now getting the points for polygon 1
-cout << "enter the point values:" << endl;
-fillPoints(Poly1, sides);
+    cout << "enter number of sides:" << endl;
+    cin >> sides;
+    Polygon Poly1(sides);
+    cout << "enter the point values:" << endl;
+    fillPoints(Poly1, sides);
 
-//now for the second shape
-cout << "enter number of sides:" << endl;
-cin >> sides;
-//putting it in the assignment ctor
-Polygon Poly2(sides);
-//get the point for polygon 2
-cout << "enter the point values:" << endl;
-fillPoints(Poly2, sides);
-   
-// Compare polygons and print results
-if (Poly1.isIdentical(Poly2)) 
-{
-cout << "equal" << endl;
-cout << "perimeter: " << round(Poly1.perimeter()) << endl;
-} 
-else 
-{
-cout << "not equal" << endl;
-cout << "perimeter: "<< round(Poly1.perimeter()) << endl;
-cout << "perimeter: "<< round(Poly2.perimeter()) << endl;
-}
-return 0;
+    cout << "enter number of sides:" << endl;
+    cin >> sides;
+    Polygon Poly2(sides);
+    cout << "enter the point values:" << endl;
+    fillPoints(Poly2, sides);
+
+    // Get a shallow copy of points for Poly1
+    Point* pointsCopy = Poly1.getPoints();
+    // Delete the shallow copy to trigger the destructor
+    delete[] pointsCopy;
+
+    if (Poly1.isIdentical(Poly2)) {
+        cout << "equal" << endl;
+        cout << "perimeter: " << round(Poly1.perimeter()) << endl;
+    } else {
+        cout << "not equal" << endl;
+        cout << "perimeter: " << round(Poly1.perimeter()) << endl;
+        cout << "perimeter: " << round(Poly2.perimeter()) << endl;
+    }
+    return 0;
 }
 
 void fillPoints(Polygon& shape, int size) {
-   int x;
-   int y;
-   char par;
-for(int i = 0; i < size; ++i)
-{
-   //put the x and y and coordinate format
-   cin >> par >> x >> par >> y >> par;
-   shape.getPoints()->setX(x);
-   shape.getPoints()->setY(y);
-}
+    int x, y;
+    char par;
+    for (int i = 0; i < size; ++i) {
+        cin >> par >> x >> par >> y >> par;
+        Point p(x, y);
+        shape.setPoint(p, i);
+    }
 }
