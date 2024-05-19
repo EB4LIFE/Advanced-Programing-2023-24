ffe51f9c75d0b0866d29df60497e24a7fa5864fc
EB4LIFE <105313463+EB4LIFE@users.noreply.github.com>
2024-05-19 06:48:40 -0400

    Update EBAPHW2Point.h

--- EBAPHW2Point.h
+++ EBAPHW2Point.h
@@ -9,18 +9,26 @@ using namespace std;
 
 class Point {
 private:
+//private by default
+//x and y values for the coord in polygon 
     float x;
     float y;
 
 public:
     // Default constructor
-    Point() : x(0), y(0) {}
+    // set x and y to default 0
+    Point() : x(0), y(0) {
+    }
 
-    // Assignment constructor
-    Point(float x, float y) : x(x), y(y) {}
-
-    // Copy constructor
-    Point(const Point& p) : x(p.x), y(p.y) {}
+    // Assignment constructor 
+    //setting x and y to user x and y will be 
+    //better implemented via setter and getter
+    Point(float x, float y) : x(x), y(y) {
+    }
+    // Copy constructor - copy of assignment so it can help for 
+    //a second object of same value
+    Point(const Point& p) : x(p.x), y(p.y) {
+    }
 
     // Getter for x
     float getX() const {
@@ -34,17 +42,20 @@ public:
 
     // Setter for x
     void setX(float x) {
+        //this is used to define the og x variable to the one brough in the assignment ctor
         this->x = x;
     }
 
     // Setter for y
     void setY(float y) {
+         //this is used to define the og y variable to the one brough in the assignment ctor
         this->y = y;
     }
-
-    // Method to calculate distance between two points
+    // Method to calculate distance between two points aka simple distance formula
     float distance(const Point& p) const {
+        //sqaure root of x2 - x1 squared + y2 - y1 sqaured
         return sqrt(pow(x - p.x, 2) + pow(y - p.y, 2));
     }
 };
+
 #endif
