538de5255150524c4e819d3e125f692aa45c085b
EB4LIFE <105313463+EB4LIFE@users.noreply.github.com>
2024-05-23 09:15:51 +0300

    Update EPHW3Dates.h

--- EPHW3Dates.h
+++ EPHW3Dates.h
@@ -12,20 +12,60 @@ private:
 //by default
 int day; 
 //of month (basically 1 - 31)
+
 int month;
 //of year (basically 1 -12)
+
 int year;
-//From Year zero CE (basically zero and up)
+//From Year zero CE (basically from defualt year to 9999
+
+//For Feb which has 28 days reg but 29 in leap 
+bool isLeapYear(int year) const {
+        return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
+    }
+//if date inputed is valid
+bool isValid(int day, int month, int year) const {
+  
+}
+
+
 public:
 
 //Assingment ctor
-Date (int dday = 1, int dmonth = 1, int dyear = 1582) : day(dday), month(dmonth), year(dyear) {
-  //check the validity of the date according to the rules described here
-}
+Date (int dday, int dmonth, int dyear) : day(dday), month(dmonth), year(dyear) {
+  if ((dday < 1) || (dday > 31)) {
+		cout << "Error day" << end;
+    //assigning default value
+		day = 1; 
+	}
+	if ((dmonth < 1) || (dmonth > 12)) { 
+		cout << "Error month" << endl;
+    //assigning default value
+		month = 1; 
+	}
+	if ((year < 1582) || (year > 9999)) { 
+		cout << "Error year" << endl;
+    //assigning default value
+		year = 1582; 
+	}
+
+
 //copy since we are dealing with date will cal it fruit cuz i'm funny
-Date (const Date &fruit) {
+Date (const Date &fruit) : day(fruit.dday), month(fruit.dmonth), year(fruit.dyear) {
 
 }
+
+void setDate(int d, int m, int y) {
+  //if values inputed are truly valid 
+  //we are good to say them as value
+       
+        }
+    }
+
+void print () const {
+cout << day << "/" << month << "/" << year << endl;
+}
+
 
 };
 #end if //DATE_H
