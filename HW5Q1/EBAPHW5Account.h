293857c7772bb62357c7db077ca09d0e2c9472ca
EB4LIFE <105313463+EB4LIFE@users.noreply.github.com>
2024-06-10 02:05:18 -0400

    Update EBAPHW5Account.h

--- HW5Q1/EBAPHW5Account.h
+++ HW5Q1/EBAPHW5Account.h
@@ -6,155 +6,125 @@
 #include <iostream>
 using namespace std;
 
-
-
+//eitanbrown346816549
 class Account {
-//private by default
 private:
-
 //bank account numbers
 int accountNumber;
 //code for account - 4 dgits (leftmost not zero)
 int code;
 //balance of account
-float balance;
-//a correct email address consists of
+int balance;
+//a correct eemailaddress consists of
 //@ with preceding characters with no space, and after the @ sign, characters
 //with no space, ending with one of the following: .com or .co.il.
-string mail;
+string email;
 //add static variable for methods
-
-static int sumDeposit;
 static int sumWithdraw;
+static int sumDeposit;
 
 public:
-//ctor - set all to zero and string to empty
-Account () {
-    accountNumber = 0;
-    code = 0;
-    balance = 0.0;
-    mail = "";
-}
-//assignment ctor w/ parametrs 
-Account (int AccNum, int codeword, float money, string mailb) : accountNumber(AccNum), code(codeword), balance(money), mail(mailb) {
-   
-}
+    // Constructors
+    Account() : accountNumber(0), code(0), balance(0), email("") {
+        //nothing to see here :0
+    }
+    //assignment constructor – receives a value for each field and assigns them
+    //accordingly. If any of the values is invalid:
 
-//setters and getters
-void setBal(float money) {
-   balance = money;
-}
-void setMail(string mailb) {
-//as stated next to variable above we need to check if email is valid 
-//@ with preceding characters with no space, and after the @ sign, characters
-//with no space, ending with one of the following: .com or .co.il.
-try {
-   int index;
-   //checking if the email contains an @ symbol
-	index = mailb.find('@'); 
-	   //We will exit code from find function if char not found
-		if (index == -1) throw string("wrong email!"); 
-		//...ending with one of the following: .com or .co.il.
-		if ((mailb.find(".com", index) == -1) || (mailb.find(".co.il", index) == -1)) { 
-		   //if not there then ...
-			throw string("wrong email!");
-		}
-		//no issues so set as normal
-		mail = mailb;
-   
-}
-//catches error message from try and prints it
-catch (string s) {
-   cout << s << endl;
-	return;
-}
+    Account(int accNum, int c, int bal, const string &em) {
+        if (c < 1000 || c > 9999) {
+            throw "ERROR: code must be of 4 digits!\n";
+        }
+        //-1 is not found via askiiiiiiiiiiiiiiiiiiiiiii
+        if (em.find('@') == -1) {
+            throw "ERROR: email must contain @!\n";
+        }
+        //meaning not found
+        if (!(em.find(".com") != -1 || em.find(".co.il") != -1)) {
+            throw "ERROR: email must end at .com or .co.il!\n";
+        }
+        accountNumber = accNum;
+        code = c;
+        balance = bal;
+        email = em;
+    }
 
-}
+    // Getters
+    int getAccountNumber() const { 
+        return accountNumber; 
+    }
+    int getCode() const { 
+        return code; 
+    }
+    int getBalance() const { 
+        return balance; 
+    }
+    string getEmail() const { 
+        return email; 
+    }
 
-int getAccNum() const {
-   return accountNumber;
-}
-int getCode() const {
-   return code;
-}
-float getBal() const {
-   return balance;
-}
-string getMail() const {
-   return mail;
-}
-//static methods voids are essentials setters and static int are getters (return etc)
-//method to withdraw a given amount of money from an account
-void withdraw(int nis, Clock clocker) {
-   //This will check withdrawl
-   //If he withdraws and he gets below -6000 which oversteps overdraft limits throw 6000
-   if ((balance - nis) < -6000) throw 6000; 
-   //if he tries to withdraw more than limit of 2500 throw 2500
-	if (nis > 2500) throw 2500; 
-	//at this point everything was smooth and was inbounds so edit account accordingly
-	balance -= nis; 
-	sumWithdraw += nis; 
-}
-//method to deposit
-void deposit(int nis, Clock clocker) {
-   //tries to deposit more than 1000 shek at a time..
-   if (nis > 10000) throw string("ERROR: cannot deposit more than 10000 NIS!"); 
-	//at this point everything was smooth and was inbounds so edit account accordingly
-	balance += nis; 
-	sumDeposit += nis; 
-}
-//as static const is not needed
-static int getSumWithdraw() {
-   return sumWithdraw; 
-}
-static int getSumDeposit() {
-   return sumDeposit;
-} 
-//friend input cin operator
-friend istream& operator>>(istream& is, Account& bank) {
-   //setting a code and email to bank account 
-   int codeword;
-	string mailb;
-	is >> bank.accountNumber >> codeword >> mailb;
-	
-	//now we check if code is 4 digits - valdiity check on input 
-	int tester = codeword / 1000;
-	//Meaning if lesss than zero it is less than 4 digits 
-	//greater than 10 it is more than 4 digits 
-	//both bad so we throw that shiii
-	if ((tester <= 0) || (tester >= 10)) { 
-		throw string("ERROR: code must be of 4 digits!");
-	}
-	//if we gutchi then we set the code like normal
-	else 
-	{
-		bank.code = codeword;
-	}
-	//now we check if email is correct like we did earlier 
-	//if find returns -1 the char not found 
-	//no comments as we did this same process above
-	int e = mailb.find('@');    
-	
-	if (e == -1) throw string("ERROR: email must contain @"); 
-	//now the com and il error checks 
-	int com = mailb.find(".com");
-	int il = mailb.find(".co.il"); 
-	if ((com == -1) && (il == -1)) { 
-		throw string("ERROR: email must end at .com or .co.il!");
-	}
-	//meaning if a positioin was returned in find() and not -1 we set as normal
-	else {
-		bank.mail = mailb;
-	}
-   //since new account balance gets set to 0 default 
-	bank.balance = 0;
-	return is;
-	
-	}
+    static int getSumWithdraw() { 
+            return sumWithdraw; 
+    }
+    static int getSumDeposit() { 
+        return sumDeposit; 
+    }
 
+    // Input operator
+    friend istream& operator>>(istream &in, Account &acc) {
+        int accNum;
+        int c;
+        string em;
+        //cout << "Enter account number: ";
+        in >> accNum;
+        //cout << "Enter code: ";
+        in >> c;
+        //cout << "Enter email: ";
+        in >> em;
+        //same as in assignment ctor
+        if (c < 1000 || c > 9999) {
+            throw "ERROR: code must be of 4 digits!\n";
+        }
+        //-1 is not found via askiiiiiiiiiiiiiiiiiiiiiii
+        if (em.find('@') == -1) {
+            throw "ERROR: email must contain @!\n";
+        }
+        //meaning not found
+        if (!(em.find(".com") != -1 || em.find(".co.il") != -1)) {
+            throw "ERROR: email must end at .com or .co.il!\n";
+        }
+        //otherwise add values and return the full input to data
+        acc.accountNumber = accNum;
+        acc.code = c;
+        acc.email = em;
+        return in;
+    }
+
+    // Methods
+    void withdraw(int nis) {
+        //bad overflow limit
+        if (balance - nis < -6000) {
+           throw 6000;
+        //can't take out too much
+        } else if (nis > 2500) {
+            throw 2500;
+        } else {
+            balance -= nis;
+            sumWithdraw+=nis;
+        }
+    }
+    //an't put in more than 1000shek 
+    void deposit(int nis) {
+        if (nis > 10000) {
+            throw string("ERROR: cannot deposit more than 10000 NIS!\n");
+        } else {
+            balance += nis;
+            sumDeposit+=nis;
+        }
+    }
 };
-// Definition of static member variables
-int Account::sumDeposit = 0;
-int Account::sumWithdraw = 0;
 
+// Initialize static fields
+int Account::sumWithdraw = 0;
+int Account::sumDeposit = 0;
 #endif //ACCOUNT_H
