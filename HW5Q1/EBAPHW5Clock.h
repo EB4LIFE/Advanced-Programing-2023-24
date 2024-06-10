f1e632e48921e070a77592e366120f19071abc2f
EB4LIFE <105313463+EB4LIFE@users.noreply.github.com>
2024-06-10 02:06:17 -0400

    Update EBAPHW5Clock.h

--- HW5Q1/EBAPHW5Clock.h
+++ HW5Q1/EBAPHW5Clock.h
@@ -3,110 +3,84 @@
 #define CLOCK_H
 #include <iostream>
 using namespace std;
-
-
+//eitanbrown346816549
 class Clock {
-
 private:
 //by default
-int hour;
-int minute; 
-int second;
+    int hour;
+    int minute;
+    int second;
 
 public:
+    // Assignment Constructor - don't need try and catch since it is in the assignctor itself
+    Clock(int h = 0, int m = 0, int s = 0) : hour(0), minute(0), second(0) {
+        //Throws for errors
+        //second exceptions
+        if (s < 0) throw "Invalid time - negative number of seconds.";
+        if (s >= 60) throw "Invalid time - more than 60 seconds.";
+        //minutes excepttions
+        if (m < 0) throw "Invalid time - negative number of minutes.";
+        if (m >= 60) throw "Invalid time - more than 60 minutes.";
+        //hour exceptions
+        if (h < 0) throw "Invalid time - negative number of hours.";
+        if (h >= 24) throw "Invalid time - more than 24 hours.";
+        //default time set to if just 1 of inputed values was invalid
+        hour = h;
+        minute = m;
+        second = s;
+    }
 
-//ctor
-Clock() {
-   hour = 00;
-   minute = 00;
-   second = 00;
-}
-//assignment ctor 
-Clock(int hours, int min, int sec) : hour(hours), minute(min), second(sec) {
-    //set parameters for max and error check for variables
-    try {
-		if ((hour > 23) || (hour < 0)) {
-			throw 'h'; //throwing error for invalid hour input
-		}
-		if ((minute > 59) || (minute < 0)) {
-			throw 'm'; //throwing error for invalid minute input
-		}
-		if ((second > 59) || (second < 0)) {
-			throw 's'; //throwing error for invalid second input
-		}
-	}
-		catch (char c) { //catching errors for invalid time input
-		//will go through all possible error types and provide the correct error message
-		if (c == 'h') {
-			if (hour > 23) { 
-				cout << "Invalid time – more than 24 hours" << endl;
-			}
-			if (hour < 0) {
-				cout << "Invalid time – negative number of hours" << endl;
-			}
-		}
-		if (c == 'm') {
-			if (minute > 60) {
-				cout << "Invalid time – more than 60 minutes" << endl;
-			}
-			if (minute < 0) {
-				cout << "Invalid time – negative number of minutes" << endl;
-			}
-		}
-		if (c == 's') {
-			if (second > 60) {
-				cout << "Invalid time – more than 60 seconds" << endl;
-			}
-			if (second < 0) {
-				cout << "Invalid time – negative number of seconds" << endl;
-			}
-		}
-		hour = 00;
-		minute = 00;
-		second = 00; 
-		//default time set to if just 1 of inputed values was invalid
-	}
-}
-//copy
-Clock(Clock& time) {
-    hour = time.hour;
-    minute = time.minute;
-    second = time.second;
-}
+    // Getters
+    int getHour() const { 
+        return hour; 
+    }
+    int getMinute() const {
+      return minute; 
+      }
+    int getSecond() const { 
+        return second; 
+    }
 
-//setters and getters
-void setHour(int hours) 
-{
-    //similiar to assignment ctor
-try {   
+    // Setters
+
+    void setHour(int h) {
+        //check if hour is valid before set 
+        //if (h < 0 || h >= 24) throw "Invalid time - hour out of range.";
+        //hour = h;
+         //similiar to assignment ctor
+        try {   
         //if user entered invalid hour input, throw the value to catch
-		if ((hours > 23) || (hours < 0)) throw hours; 
-		hour = hours;
+		if ((h > 23) || (h < 0)) throw h; 
+        //otherwise set as normal
+		hour = h;
 	}
 	//now check: which error message to execute
-	catch (int x) { 
-		if (x > 23) {
+	    catch (int x) { 
+		    if (x > 23) {
 			cout << "Invalid time – more than 24 hours" << endl;
-		}
-		if (x < 0) {
+		    }
+		    if (x < 0) {
 			cout << "Invalid time – negative number of hours" << endl;
-		}
-		//defualt set if  1 invalid input was put in
-		hour = 00;
-		minute = 00;
-		second = 00; 
-	}
-}
-void setMin(int min)
-{
-    //similiar to assignment ctor
-    try {
+		    }
+		    //defualt set if  1 invalid input was put in
+		    hour = 00;
+		    minute = 00;
+		    second = 00; 
+	}   
+    }
+
+    void setMinute(int m) {
+        //if (m < 0 || m >= 60) throw "Invalid time - minute out of range.";
+        //minute = m;
+        //similiar to assignment ctor
+        try {
          //if user entered invalid hour input, throw the value to catch
-		if ((min > 59) || (min < 0)) throw min; 
-		minute = min;
+		if ((m > 59) || (m < 0)) throw m; 
+        //otherwise
+		minute = m;
 	}
 	//now check: which error message to execute
-	catch (int x) { 
+	    catch (int x) { 
 		if (x > 60) {
 			cout << "Invalid time – more than 60 minutes" << endl;
 		}
@@ -118,18 +92,19 @@ void setMin(int min)
 		minute = 00;
 		second = 00;
 	}
+    }
 
-}
-void setSec(int sec)
-{   
-    //similiar to assignment ctor
-    try {
+    void setSecond(int s) {
+        //if (s < 0 || s >= 60) throw "Invalid time - second out of range.";
+        //second = s;
+            //similiar to assignment ctor
+        try {
     //if user entered invalid hour input, throw the value to catch
-		if ((sec > 59) || (sec < 0)) throw sec; 
-		second = sec;
+		if ((s > 59) || (s < 0)) throw s; 
+		second = s;
 	}
 	//now check: which error message to execute
-	catch (int x) { 
+	    catch (int x) { 
 		if (x > 60) {
 			cout << "Invalid time – more than 60 seconds" << endl;
 		}
@@ -141,48 +116,60 @@ void setSec(int sec)
 		minute = 00;
 		second = 00; 
 	}
-}
-int getHour() const
-{
-    return hour;
-}
-int getMin() const
-{
-    return minute;
-}
-int getSec() const
-{
-    return second;
-}
-//add immediate operation overload method
-//add another sec variable 
-Clock operator+=(int sec2) {
-    second += sec2; 
-    //if new value for seconds is too big
-    //We put them into minute so can get rid of them in seconds
-	if (second > 59) { 
-		++minute; 
-		second -= 60; 
-        //same process from minute to hours
-		if (minute > 59) { 
-			++hour;
-			minute -= 60;
-            //same process as above except nothing to add to so we just move hour back to 0
-			if (hour > 23) { 
-				hour = 00; 
-			}
-		}
-	}
-	return *this;
-}
+    }
+
+    // equals method using a copy object instance 
+    bool equals(const Clock& other) const {
+        return hour == other.hour && minute == other.minute && second == other.second;
+    }
+
+    // before method
+    //– A boolean method that receives a Clock instance and returns true if the
+    //time in it is later than the time in the calling instance
+    bool before(const Clock& other) const {
+        if (hour < other.hour) return true;
+        if (hour > other.hour) return false;
+        if (minute < other.minute) return true;
+        if (minute > other.minute) return false;
+        //finally if time the calling instance
+        return second < other.second;
+    }
+
+    // after method the literal oppisite of before method
+    //lazy so wrote this way and still efecttive
+    bool after(const Clock& other) const {
+        return !before(other) && !equals(other);
+    }
+
+    // operator+=
+    //receives an int that represents a number of seconds and updates the time accordingly.
+    Clock& operator+=(int seconds) {
+        if (seconds < 0) throw "Invalid time - negative number of seconds.";
+        second += seconds;
+        //60 second in am minute
+        while (second >= 60) {
+            second -= 60;
+            minute++;
+        }
+        //60 minutes in 1 hour
+        while (minute >= 60) {
+            minute -= 60;
+            hour++;
+        }
+        //latest is 24 but after 2359 we go to 00:00
+        while (hour >= 24) {
+            hour -= 24;
+        }
+        //return the instance
+        return *this;
+    }
     //cout friend operator
 	friend ostream& operator<<(ostream& os, const Clock &time) {
     os << (time.hour < 10 ? "0" : "") << time.hour << ":" << (time.minute < 10 ? "0" : "") << time.minute << ":" << (time.second < 10 ? "0" : "") << time.second;
     
 	return os;
 	}
-	
-	//cin friend operator
+    //cin friend operator
 	friend istream& operator>>(istream& is, Clock &time) {
 	//basic var names
 	char c;
@@ -216,6 +203,6 @@ Clock operator+=(int sec2) {
 	}
 	return is;
 	}
-
 };
-#endif //CLOCK_H
+#endif 
+
