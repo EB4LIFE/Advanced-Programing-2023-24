730661841823714219b8b754cdcb11eee69ea536
EB4LIFE <105313463+EB4LIFE@users.noreply.github.com>
2024-06-12 13:41:13 -0400

    Create Rational.cpp

---
+++ HW1OOPQ1/Rational.cpp
@@ -0,0 +1,102 @@
+#include "Rational.h"
+
+// The required constructors
+// An assignment constructor (ctor) with parameters that default to 0 for
+// the numerator and 1 for the denominator. The implementation must 
+// contain the following output : print: in constructor
+Rational::Rational(int num, int den) : numerator(num), denominator(den) {
+    cout << "print: in constructor" << endl;
+    if (den == 0) {
+        denominator = 1;
+    } else {
+        denominator = den;
+    }
+    if (denominator < 0) {
+        numerator = -numerator;
+        denominator = -denominator;
+    }
+}
+
+// Copy constructor
+Rational::Rational(const Rational& other) : numerator(other.numerator), denominator(other.denominator) {
+    cout << "print: in copy-constructor" << endl;
+}
+
+// Set numerator
+void Rational::setNumerator(int num) {
+    numerator = num;
+}
+
+// Set denominator
+void Rational::setDenominator(int den) {
+    if (den == 0) {
+        denominator = 1;
+    } else {
+        denominator = den;
+    }
+    if (denominator < 0) {
+        numerator = -numerator;
+        denominator = -denominator;
+    }
+}
+
+// Get numerator
+int Rational::getNumerator() const {
+    return numerator;
+}
+
+// Get denominator
+int Rational::getDenominator() const {
+    return denominator;
+}
+
+// Method to print the rational number
+void Rational::print() const {
+    cout << numerator << "/" << denominator;
+}
+
+// Method to check equality
+bool Rational::equal(const Rational other) const {
+    return numerator == other.numerator && denominator == other.denominator;
+}
+
+// Method to make two rationals equal
+void Rational::makeEquals(Rational& other) {
+    other.numerator = numerator;
+    other.denominator = denominator;
+}
+
+// Helper "stepping stone" function method for reducing the fraction
+void Rational::reduce() {
+    int gcd = getGCD(numerator, denominator);
+    numerator /= gcd;
+    denominator /= gcd;
+    if (numerator == 0) {
+        denominator = 1;
+    }
+}
+
+// Method to add another rational
+Rational Rational::add(const Rational other) const {
+    int num = numerator * other.denominator + other.numerator * denominator;
+    int den = denominator * other.denominator;
+    Rational sum(num, den);
+    sum.reduce();
+    return sum;
+}
+
+// Method to add one to the rational
+Rational Rational::addOne() const {
+    int num = numerator * 1 + 1 * denominator;
+    int den = denominator * 1;
+    Rational sum(num, den);
+    return sum;
+}
+
+// Private method to get the GCD of two numbers
+int Rational::getGCD(int a, int b) const {
+    if (b == 0) {
+        return a;
+    }
+    return getGCD(b, a % b);
+}
