2729cb402031680dd532d0c3bcf46ff69a1e4666
EB4LIFE <105313463+EB4LIFE@users.noreply.github.com>
2024-06-17 03:35:08 -0400

    Update Student.cpp

--- HW6InhertianceQ2/Student.cpp
+++ HW6InhertianceQ2/Student.cpp
@@ -1,1 +1,12 @@
+#include "Student.h"
+#include <iostream>
+
+using namespace std;
+
+// Constructor definition 
+Student::Student(int id, string firstName, string lastName, int numCourses)
+    : id(id), firstName(firstName), lastName(lastName), numCourses(numCourses) {
+      //empty
+    }
+
 
