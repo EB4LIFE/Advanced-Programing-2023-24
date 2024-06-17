d743764ae9324f24de7ce8195bac5b02d52d82bd
EB4LIFE <105313463+EB4LIFE@users.noreply.github.com>
2024-06-17 03:53:08 -0400

    Update PHD.cpp

--- HW6InhertianceQ2/PHD.cpp
+++ HW6InhertianceQ2/PHD.cpp
@@ -1,1 +1,28 @@
+#include "PHD.h"
+#include <iostream>
+
+using namespace std;
+
+// Constructor definition
+PHD::PHD(int id, string firstName, string lastName, int numCourses, int researchHours)
+    : Student(id, firstName, lastName, numCourses), researchHours(researchHours) {
+    //empty 
+}
+
+// Override function milga() definition
+bool PHD::milga() const {
+    // Check if the student has taken at least 2 courses and has more than 25 research hours with is nessary for PHD 
+    return numCourses >= 2 && researchHours > 25;
+  //for pHD no need to cheg avg
+}
+
+// Override function print() 
+void PHD::print() const {
+    
+    cout << "ID: " << id << endl;
+    cout << "First Name: " << firstName << endl;
+    cout << "Last Name: " << lastName << endl;
+    cout << "Number of Courses: " << numCourses << endl;
+    cout << "Research Hours: " << researchHours << endl;
+}
 
