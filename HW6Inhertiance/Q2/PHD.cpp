e1bd2eed895e7e26c47f18905564d4a5eb72c4f4
EB4LIFE <105313463+EB4LIFE@users.noreply.github.com>
2024-06-19 22:52:49 +0300

    Update PHD.cpp

--- HW6Inhertiance/Q2/PHD.cpp
+++ HW6Inhertiance/Q2/PHD.cpp
@@ -1,28 +1,22 @@
 #include "PHD.h"
-#include <iostream>
 
-using namespace std;
-
-// Constructor definition
-PHD::PHD(int id, string firstName, string lastName, int numCourses, int researchHours)
-    : Student(id, firstName, lastName, numCourses), researchHours(researchHours) {
-    //empty 
+// Input method
+void PHD::input() {
+    cout << "enter id, first name, last name, number of courses\n";
+    cin >> studentId >> firstName >> lastName >> numCourses;
+    cout << "enter number of research hours" << endl;
+    cin >> hoursInResearch;
 }
 
-// Override function milga() definition
-bool PHD::milga() const {
-    // Check if the student has taken at least 2 courses and has more than 25 research hours with is nessary for PHD 
-    return numCourses >= 2 && researchHours > 25;
-  //for pHD no need to cheg avg
-}
-
-// Override function print() 
+// Print method
 void PHD::print() const {
-    
-    cout << "ID: " << id << endl;
-    cout << "First Name: " << firstName << endl;
-    cout << "Last Name: " << lastName << endl;
-    cout << "Number of Courses: " << numCourses << endl;
-    cout << "Research Hours: " << researchHours << endl;
+    cout << "ID:\t" << studentId << endl;
+    cout << "Name:\t" << firstName << " " << lastName << endl;
+    //cout << "num courses: " << numCourses << endl;
+    cout << "number of research Hours:\t" << hoursInResearch << endl;
 }
 
+// Grant eligibility method
+bool PHD::milga() const {
+    return numCourses >= 2 && hoursInResearch > 25;
+}
