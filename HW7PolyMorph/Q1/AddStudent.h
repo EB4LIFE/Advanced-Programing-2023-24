14f446ed437c8fa297a7df835251b793b097826d
EB4LIFE <105313463+EB4LIFE@users.noreply.github.com>
2024-06-23 22:19:21 +0300

    Update AddStudent.h

--- HW7PolyMorph/Q1/AddStudent.h
+++ HW7PolyMorph/Q1/AddStudent.h
@@ -1,9 +1,51 @@
 #ifndef ADDSTUDENT_H
 #define ADDSTUDENT_H
-
 #include "Student.h"
+#include "BA.h"
+#include "MA.h"
+#include "PHD.h"
+#include <exception> 
+#include <iostream>
+using namespace std;
 
-//the add student declared here
-Student* addStudent();
+
+// Function to add a student
+//The object of the function is to allow the user to choose the type of student to
+//input (1 for BA, 2 for MA and 3 for PHD) and to return the address of a new
+//dynamic allocation of the correct type.
+//After the dynamic allocation the method must call the input method so that the
+//program will input student data the matches the student type.
+//If the user chooses an invalid choice the function must throw an exception.
+
+Student* addStudent() {
+    int choice;
+    cout << "Enter degree: 1 for BA, 2 for MA, or 3 for PhD" << endl;
+    cin >> choice;
+
+    // Create a pointer to Student
+    Student* student = nullptr;
+  
+    // Determine the type of student to create
+    switch (choice) {
+        case 1:
+            student = new BA();
+            break;
+        case 2:
+            student = new MA();
+            break;
+        case 3:
+            student = new PHD();
+            break;
+        default:
+           throw string("Exception: no such degree\n");
+    }
+    // Call input method to input student data either PHD, MA, BA
+
+    student->input();
+
+      
+    //returns DMA adress of students   
+    return student; 
+}
 
 #endif // ADDSTUDENT_H
