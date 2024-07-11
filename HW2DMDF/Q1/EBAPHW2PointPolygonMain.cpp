6c38a16e3381339817e30c5aa4f9d8e61c428bc2
EB4LIFE <105313463+EB4LIFE@users.noreply.github.com>
2024-07-11 12:56:09 +0300

    Update EBAPHW2PointPolygonMain.cpp

--- HW2DMDF/Q1/EBAPHW2PointPolygonMain.cpp
+++ HW2DMDF/Q1/EBAPHW2PointPolygonMain.cpp
@@ -5,7 +5,23 @@
 #include "Point.h"
 #include "Polygon.h"
 using namespace std;
-
+/*
+Implement a main program that inputs data pertaining to two polygons and prints their
+perimeters, rounded to the closest integer (you may make use of the function round in
+the library cmath), as follows:
+a. If the polygons are identical the program must print “equal” and on the next
+line print the perimeter (shared by both).
+b. If the polygons are not identical the program must print “not equal” and on the
+next lines the perimeters – each in a separate line.
+The input of the polygons will consist of the output “enter number of sides:” followed
+by the input of the number of points. Then the output “enter the point values:”
+followed by the input of the coordinates.
+The coordinate input will be of the format: (x1,y1) (x2,y2) … (xN,yN)
+Where N is the number of points in the polygon and (xi,yi) are the point coordinates.
+After the input of each Point it must be saved in its place in the polygon using the
+method setPoint.
+For any invalid input the program will print “ERROR”.
+*/
 void fillPoints(Polygon& shape, int size);
 
 int main() {
