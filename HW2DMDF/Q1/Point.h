f6c4b274a9558713e0134ec45434cef107a97639
EB4LIFE <105313463+EB4LIFE@users.noreply.github.com>
2024-07-11 12:45:36 +0300

    Update Point.h

--- HW2DMDF/Q1/Point.h
+++ HW2DMDF/Q1/Point.h
@@ -10,17 +10,28 @@ using namespace std;
 
 class Point {
 private:
+/* x – a floating point number for the x coordinate
+y – a floating point number for the y coordinate */
     float x;
     float y;
 
 public:
+    //ctor
     Point();
+
+    //param ctor
     Point(float x, float y);
+
+    //copy ctor
     Point(const Point& p);
+
+    //getters and setters
     float getX() const;
     float getY() const;
     void setX(float x);
     void setY(float y);
+
+    //helper method
     float distance(const Point& p) const;
 };
 
