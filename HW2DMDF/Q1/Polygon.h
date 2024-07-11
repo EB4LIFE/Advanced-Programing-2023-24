48a4d95733c631fbd608f8a9313fff4d62df8434
EB4LIFE <105313463+EB4LIFE@users.noreply.github.com>
2024-07-11 12:48:28 +0300

    Update Polygon.h

--- HW2DMDF/Q1/Polygon.h
+++ HW2DMDF/Q1/Polygon.h
@@ -10,17 +10,33 @@ using namespace std;
 
 class Polygon {
 private:
+/*
+A pointer to the address of a Point array of unspecified size.
+The number of points in the polygon (the size of the array).
+*/
     Point* points;
     int numPoints;
 
 public:
+    //ctor
     Polygon();
+
+    //param ctor
     Polygon(int numPoints);
+
+    //copy ctor
     Polygon(const Polygon& poly);
+
+    //destructor for pointer
     ~Polygon();
+
+    //getters and setters
+    //type is Point* becuase of type in private
     Point* getPoints() const;
     int getNumPoints() const;
     void setPoint(const Point& p, int ind);
+
+    //Helper methods
     float perimeter() const;
     bool isIdentical(Polygon poly);
 };
