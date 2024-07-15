f782fd941349b93ab9f5853f87e6ba2da39f49af
EB4LIFE <105313463+EB4LIFE@users.noreply.github.com>
2024-07-15 13:33:02 +0300

    Update Main.cpp

--- HW10Trees/Q4/Main.cpp
+++ HW10Trees/Q4/Main.cpp
@@ -1,1 +1,45 @@
+#include <iostream>
+using namespace std;
+#include "SearchTree.h"
+#include "Student.h"
+
+int main() {
+    SearchTree<Student> sList;
+    Student tmp;
+    char choice = 'i';
+
+    while (choice != 'e') {
+        cout << "enter a-e\n";
+        cin >> choice;
+        switch (choice) {
+        case 'a':
+            cout << "enter a student\n";
+            cin >> tmp;
+            sList.add(tmp);
+            break;
+        case 'b':
+            cout << "enter a student\n";
+            cin >> tmp;
+            sList.remove(tmp);
+            break;
+        case 'c':
+            cout << "enter a student\n";
+            cin >> tmp;
+            if (sList.search(tmp))
+                cout << "exist\n";
+            else
+                cout << "not exist\n";
+            break;
+        case 'd':
+            sList.inOrder();
+            break;
+        case 'e':
+            break;
+        default:
+            cout << "error\n";
+            break;
+        }
+    }
+    return 0;
+}
 
