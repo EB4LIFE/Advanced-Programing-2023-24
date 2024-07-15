44a131e44d609e97552132ffe320b68edcada480
EB4LIFE <105313463+EB4LIFE@users.noreply.github.com>
2024-07-15 13:31:55 +0300

    Create Student.h

---
+++ HW10Trees/Q4/Student.h
@@ -0,0 +1,31 @@
+#ifndef STUDENT_H
+#define STUDENT_H
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Student {
+private:
+    int id;
+    string lastName;
+    string firstName;
+
+public:
+    Student();
+    Student(int id, string lastName, string firstName);
+    Student(const Student& other);
+    Student(Student&& other) noexcept;
+    ~Student();
+
+    Student& operator=(const Student& other);
+    Student& operator=(Student&& other) noexcept;
+
+    bool operator<=(const Student& other) const;
+    bool operator==(const Student& other) const;
+
+    friend ostream& operator<<(ostream& os, const Student& student);
+    friend istream& operator>>(istream& is, Student& student);
+};
+
+#endif // STUDENT_H
